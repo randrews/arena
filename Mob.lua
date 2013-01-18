2d95a9e9d0ae66a6ebd9bbe3f7408de2b2b021ad
Ross Andrews <randrews@geekfu.org>
2013-01-18 01:17:45 -0600

    Added mobs that chase you around

---
+++ Mob.lua
@@ -0,0 +1,106 @@
+require 'middleclass'
+
+Mob = class('Mob')
+
+function Mob:initialize(world, location, target, range, speed)
+   assert(world and target and location)
+
+   self.target = target
+   self.range = range or 0
+   self.speed = speed or 50
+
+   assert(self.speed > 0 and self.speed <= 100)
+
+   local b = love.physics.newBody(world, location.x, location.y, 'dynamic')
+   local s = love.physics.newCircleShape(16)
+   love.physics.newFixture(b, s)
+   b:setMass(1)
+   b:setAngularDamping(3)
+   b:setLinearDamping((110 - self.speed) / 5)
+
+   self.turn_speed = 100 * (self.speed) / 5
+   self.accel = 640
+
+   self.body=b
+   self.shape=s
+
+   b:setAngle(self:angle_to_target())
+
+   self.color = {110, 140, 190}
+end
+
+function Mob:draw()
+   local g = love.graphics
+   local b = self.body
+   local s = self.shape
+
+   g.setColor(unpack(self.color))
+   g.circle('fill', b:getX(), b:getY(), s:getRadius(), 20)
+
+   g.setColor(220, 220, 90)
+   g.line(b:getX(),b:getY(),
+          b:getX() + 50 * math.cos(b:getAngle()),
+          b:getY() + 50 * math.sin(b:getAngle()))
+
+end
+
+function Mob:distance_to_target()
+   local dx = self.body:getX() - self.target.body:getX()
+   local dy = self.body:getY() - self.target.body:getY()
+   return math.sqrt(dx*dx+dy*dy)
+end
+
+function Mob:angle_to_target()
+   local dx = self.body:getX() - self.target.body:getX()
+   local dy = self.body:getY() - self.target.body:getY()
+   local angle = math.atan(dy/dx) + math.pi/2
+   if dx < 0 then angle = angle + math.pi end
+   return angle + math.pi / 2
+end
+
+function Mob:turn_toward_target()
+   local ta = self:angle_to_target()
+   local a = self.body:getAngle()
+   local ccw = (a - ta) % (math.pi * 2)
+   local cw = (math.pi * 2) - ccw
+   local thresh = math.pi / 180 -- one degree
+
+   if cw < thresh or ccw < thresh then
+      self.body:setAngularVelocity(0)
+   elseif cw < ccw then
+      self.body:applyTorque(self.turn_speed)
+   else
+      self.body:applyTorque(-self.turn_speed)
+   end
+end
+
+-- Limits mob max linear speed
+function Mob:max_speed()
+   local max = 600
+   local x, y = self.body:getLinearVelocity()
+   if x*x + y*y > max*max then
+      local a = math.atan2(y,x)
+      self.body:setLinearVelocity(max * math.cos(a),
+                                  max * math.sin(a))
+   end
+end
+
+function Mob:update()
+   local dist = self:distance_to_target()
+   local accel = self.accel
+
+   self:turn_toward_target()
+
+   if dist > self.range + 5 then
+      self.body:applyForce(math.cos(self.body:getAngle()) * accel,
+                           math.sin(self.body:getAngle()) * accel)
+
+   elseif dist < self.range - 5 then
+      self.body:applyForce(math.cos(self.body:getAngle()) * -accel,
+                           math.sin(self.body:getAngle()) * -accel)
+
+   else
+   end
+
+   self:max_speed()
+end
--- main.lua
+++ main.lua
@@ -2,10 +2,12 @@ require('middleclass')
 require('Point')
 require('Player')
 require('Crate')
+require('Mob')
 
 local world = nil
 local player = nil
 local crates = nil
+local mobs = nil
 
 function love.load()
    math.randomseed(os.time())
@@ -16,15 +18,22 @@ function love.load()
    player = Player(world)
    crates = { Crate(world, 5, 5),
               Crate(world, 5, 6) }
+   mobs = { Mob(world, Point(100, 100), player, 150, 50),
+            Mob(world, Point(100, 100), player, 300, 10),
+            Mob(world, Point(100, 100), player, 80, 95),
+            Mob(world, Point(100, 100), player, 80, 95),
+            Mob(world, Point(100, 100), player, 80, 95), }
 end
 
 function love.draw()
    player:draw()
    for _, c in ipairs(crates) do c:draw() end
+   for _, m in ipairs(mobs) do m:draw() end
 end
 
 function love.update(dt)
    player:update(dt)
    for _, c in ipairs(crates) do c:update(dt) end
+   for _, m in ipairs(mobs) do m:update(dt) end
    world:update(dt)
 end
